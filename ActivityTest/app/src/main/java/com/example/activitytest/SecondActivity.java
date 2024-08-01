4dd0e75212a6c5b0ecdb3092bb6bb4aa58f9cd95
fansuregrin <pwz113436@gmail.com>
2024-08-01 15:22:57 +0800

    Add: singleTop launch mode

--- ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
+++ ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -1,15 +1,30 @@
 package com.example.activitytest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
     private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate " + this.toString());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+
+        Button button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
+
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FirstActivity.class);
+            startActivity(intent);
+        });
     }
 }
