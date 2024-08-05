1ad24dba4c70cd6aecf4ee7df58f6e0d1628a43e
fansuregrin <pwz113436@gmail.com>
2024-08-05 15:24:30 +0800

    Add: RecyclerViewTest

---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/Fruit.java
@@ -0,0 +1,19 @@
+package com.example.recyclerviewtest;
+
+public class Fruit {
+    private final int nameId;
+    private final int imageId;
+
+    public Fruit(int nameId, int imageId) {
+        this.nameId = nameId;
+        this.imageId = imageId;
+    }
+
+    public int getNameId() {
+        return nameId;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.java
@@ -0,0 +1,51 @@
+package com.example.recyclerviewtest;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view) {
+            super(view);
+            fruitImage = view.findViewById(R.id.fruit_image);
+            fruitName = view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    private final List<Fruit> mFruitList;
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.fruit_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getNameId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+}
