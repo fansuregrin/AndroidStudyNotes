caa4bdad3059f5301c9237e9a80f1c18fd677448
fansuregrin <pwz113436@gmail.com>
2024-08-06 10:36:17 +0800

    Update RecyclerViewTest: add click event

--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.java
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.java
@@ -1,10 +1,12 @@
 package com.example.recyclerviewtest;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,11 +15,13 @@ import java.util.List;
 
 public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        View fruitView;
         ImageView fruitImage;
         TextView fruitName;
 
         public ViewHolder(View view) {
             super(view);
+            fruitView = view;
             fruitImage = view.findViewById(R.id.fruit_image);
             fruitName = view.findViewById(R.id.fruit_name);
         }
@@ -36,7 +40,22 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
             .inflate(mFruitItemLayout, parent, false);
-        return new ViewHolder(view);
+        ViewHolder holder = new ViewHolder(view);
+        holder.fruitView.setOnClickListener(v -> {
+            int position = holder.getAdapterPosition();
+            Fruit fruit = mFruitList.get(position);
+            Context context = v.getContext();
+            String msg = context.getString(R.string.click_view) + context.getString(fruit.getNameId());
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        });
+        holder.fruitImage.setOnClickListener(v -> {
+            int position = holder.getAdapterPosition();
+            Fruit fruit = mFruitList.get(position);
+            Context context = v.getContext();
+            String msg = context.getString(R.string.click_image) + context.getString(fruit.getNameId());
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        });
+        return holder;
     }
 
     @Override
