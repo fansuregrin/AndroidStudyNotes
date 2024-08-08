5f3cce6bde515719b412aa95a6d65399d5b32404
fansuregrin <pwz113436@gmail.com>
2024-08-08 14:55:00 +0800

    Update UIExploring: add more examples for EditText

--- UIExploring/app/src/main/java/com/example/uiexploring/EditTextActivity.java
+++ UIExploring/app/src/main/java/com/example/uiexploring/EditTextActivity.java
@@ -3,6 +3,7 @@ package com.example.uiexploring;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,13 +18,24 @@ public class EditTextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
 
-        editText = findViewById(R.id.edit_text);
+        editText = findViewById(R.id.edit_text_multiline);
         Button button1 = findViewById(R.id.button_1);
         button1.setOnClickListener(v -> {
             if (v.getId() == R.id.button_1) {
                 String inputText = editText.getText().toString();
                 Toast.makeText(this, inputText, Toast.LENGTH_SHORT).show();
             }
+        });
+
+        ((EditText)findViewById(R.id.edit_text_action_send)).setOnEditorActionListener(
+            (v, actionId, event) -> {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    handled = true;
+                    String msg = getString(R.string.send_prompt) + v.getText().toString();
+                    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                }
+                return handled;
         });
     }
 
