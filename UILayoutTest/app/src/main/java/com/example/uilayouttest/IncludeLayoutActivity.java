d0b9fec92e9a1b13bed0c3bd52b68adfadfdb921
fansuregrin <pwz113436@gmail.com>
2024-08-10 22:46:03 +0800

    Update UILayoutTest: update the ui and logic of IncludeLayoutActivity

--- UILayoutTest/app/src/main/java/com/example/uilayouttest/IncludeLayoutActivity.java
+++ UILayoutTest/app/src/main/java/com/example/uilayouttest/IncludeLayoutActivity.java
@@ -3,21 +3,49 @@ package com.example.uilayouttest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IncludeLayoutActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button titleEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_include_layout);
 
+        TextView titleText = findViewById(R.id.title_text);
+        titleText.setText(R.string.include_layout);
+
+        editText = findViewById(R.id.edit_text);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        Button titleBack = findViewById(R.id.title_back);
+        titleBack.setOnClickListener(v -> {
+            finish();
+        });
+
+        titleEdit = findViewById(R.id.title_edit);
+        titleEdit.setOnClickListener(v -> {
+            if (titleEdit.getText() == getString(R.string.edit)) {
+                editText.setEnabled(true);
+                editText.setFocusableInTouchMode(true);
+                editText.requestFocus();
+                titleEdit.setText(R.string.ok);
+            } else {
+                editText.setEnabled(false);
+                titleEdit.setText(R.string.edit);
+            }
+        });
     }
 
     public static void actionStart(Context context) {
