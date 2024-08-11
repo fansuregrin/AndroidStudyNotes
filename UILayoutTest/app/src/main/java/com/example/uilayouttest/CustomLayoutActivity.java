e159a42ff6faf17fe5fe94b0edba9b89dc7409ae
fansuregrin <pwz113436@gmail.com>
2024-08-11 21:42:43 +0800

    Update UILayoutTest: update TitleLayout

--- UILayoutTest/app/src/main/java/com/example/uilayouttest/CustomLayoutActivity.java
+++ UILayoutTest/app/src/main/java/com/example/uilayouttest/CustomLayoutActivity.java
@@ -3,11 +3,14 @@ package com.example.uilayouttest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomLayoutActivity extends AppCompatActivity {
+    private TitleLayout mTitleLayout;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,20 @@ public class CustomLayoutActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        mEditText = findViewById(R.id.edit_text);
+        mTitleLayout = findViewById(R.id.title_layout);
+        mTitleLayout.setTitleEditOnClickListener(v -> {
+            if (mTitleLayout.getTitleEditText() == getString(R.string.edit)) {
+                mEditText.setEnabled(true);
+                mEditText.setFocusableInTouchMode(true);
+                mEditText.requestFocus();
+                mTitleLayout.setTitleEditText(R.string.ok);
+            } else {
+                mEditText.setEnabled(false);
+                mTitleLayout.setTitleEditText(R.string.edit);
+            }
+        });
     }
 
     public static void actionStart(Context context) {
--- UILayoutTest/app/src/main/java/com/example/uilayouttest/TitleLayout.java
+++ UILayoutTest/app/src/main/java/com/example/uilayouttest/TitleLayout.java
@@ -2,25 +2,64 @@ package com.example.uilayouttest;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TitleLayout extends LinearLayout {
+    private final TextView mTitleText;
+    private final Button mTitleBack;
+    private final Button mTitleEdit;
 
     public TitleLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TitleLayout);
+        String title = a.getString(R.styleable.TitleLayout_title);
+        a.recycle();
+
         LayoutInflater.from(context).inflate(R.layout.title, this);
 
-        Button titleBack = findViewById(R.id.title_back);
-        Button titleEdit = findViewById(R.id.title_edit);
-        titleBack.setOnClickListener(v -> {
-            ((Activity) getContext()).finish();
-        });
-        titleEdit.setOnClickListener(v -> {
-            Toast.makeText(getContext(), R.string.click_edit_btn, Toast.LENGTH_SHORT).show();
-        });
+        mTitleText = findViewById(R.id.title_text);
+        mTitleText.setText(title);
+
+        mTitleBack = findViewById(R.id.title_back);
+        mTitleEdit = findViewById(R.id.title_edit);
+        mTitleBack.setOnClickListener(v -> ((Activity) getContext()).finish());
+        mTitleEdit.setOnClickListener(v -> Toast.makeText(
+            getContext(), R.string.click_edit_btn, Toast.LENGTH_SHORT).show());
+    }
+
+    public CharSequence getTitle() {
+        return mTitleText.getText();
+    }
+
+    public void setTitle(CharSequence title) {
+        mTitleText.setText(title);
+    }
+
+    public void setTitle(int resid) {
+        mTitleText.setText(resid);
+    }
+
+    public void setTitleEditOnClickListener(View.OnClickListener listener) {
+        mTitleEdit.setOnClickListener(listener);
+    }
+
+    public CharSequence getTitleEditText() {
+        return mTitleEdit.getText();
+    }
+
+    public void setTitleEditText(CharSequence text) {
+        mTitleEdit.setText(text);
+    }
+
+    public void setTitleEditText(int resid) {
+        mTitleEdit.setText(resid);
     }
 }
