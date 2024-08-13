38fe77f9adf19c05e9b7290a4e53330e745cb05e
fansuregrin <pwz113436@gmail.com>
2024-08-13 16:37:23 +0800

    Update UIBestPractice

--- UIBestPractice/app/src/main/java/com/example/uibestpractice/MainActivity.java
+++ UIBestPractice/app/src/main/java/com/example/uibestpractice/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.uibestpractice;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button btnSend;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initMsgs();
+        inputText = findViewById(R.id.input_text);
+        btnSend = findViewById(R.id.btn_send);
+        msgRecyclerView = findViewById(R.id.msg_recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+
+        btnSend.setOnClickListener(v -> {
+            String content = inputText.getText().toString();
+            if (!content.isEmpty()) {
+                Msg msg = new Msg(content, Msg.TYPE_SENT);
+                msgList.add(msg);
+                adapter.notifyItemInserted(msgList.size() - 1);
+                msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                inputText.setText("");
+            }
+        });
+    }
+
+    private void initMsgs() {
+        msgList.add(new Msg("Hello guy.", Msg.TYPE_RECEIVED));
+        msgList.add(new Msg("Hello, who is that?", Msg.TYPE_SENT));
+        msgList.add(new Msg("This is Tom. Nice to talking to you.", Msg.TYPE_RECEIVED));
     }
 }
---
+++ UIBestPractice/app/src/main/java/com/example/uibestpractice/MsgAdapter.java
@@ -0,0 +1,63 @@
+package com.example.uibestpractice;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+
+    static public class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+
+        public ViewHolder(View view) {
+            super(view);
+            leftLayout = view.findViewById(R.id.left_layout);
+            rightLayout = view.findViewById(R.id.right_layout);
+            leftMsg = view.findViewById(R.id.left_msg);
+            rightMsg = view.findViewById(R.id.right_msg);
+        }
+    }
+
+    private final List<Msg> mMsgList;
+
+    public MsgAdapter(List<Msg> msgList) {
+        this.mMsgList = msgList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+            .inflate(R.layout.msg_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Msg msg = mMsgList.get(position);
+        if (msg.getType() == Msg.TYPE_RECEIVED) {
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+        } else if (msg.getType() == Msg.TYPE_SENT) {
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.rightMsg.setText(msg.getContent());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMsgList.size();
+    }
+}
