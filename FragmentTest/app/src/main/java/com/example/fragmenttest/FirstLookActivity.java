252cd744133cce1fdbdfaaeb321a8100fa3b12dd
fansuregrin <pwz113436@gmail.com>
2024-08-14 16:41:52 +0800

    Update FragmentTest

--- FragmentTest/app/src/main/java/com/example/fragmenttest/FirstLookActivity.java
+++ FragmentTest/app/src/main/java/com/example/fragmenttest/FirstLookActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FirstLookActivity extends AppCompatActivity {
 
@@ -12,6 +15,14 @@ public class FirstLookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_look);
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     public static void actionStart(Context context) {
--- FragmentTest/app/src/main/java/com/example/fragmenttest/LeftFragment.java
+++ FragmentTest/app/src/main/java/com/example/fragmenttest/LeftFragment.java
@@ -7,12 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class LeftFragment extends Fragment {
+    private Button button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_left, container, false);
+        View view = inflater.inflate(R.layout.fragment_left, container, false);
+
+        button = view.findViewById(R.id.button);
+
+        FirstLookActivity activity = (FirstLookActivity) getActivity();
+        if (activity != null) {
+            button.setOnClickListener(v -> activity.replaceFragment(new AnotherRightFragment()));
+        }
+
+        return view;
+    }
+
+    public void setBtnOnClickListener(View.OnClickListener l) {
+        if (button == null) return;
+        button.setOnClickListener(l);
     }
 }
