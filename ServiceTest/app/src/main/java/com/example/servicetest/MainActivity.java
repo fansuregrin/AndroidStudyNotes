dbbfe027cb59881211810742142cc1ad0cef959b
fansuregrin <pwz113436@gmail.com>
2024-08-20 15:11:39 +0800

    Update ServiceTest: bind and unbind service

--- ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.servicetest;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +36,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnStartService.setOnClickListener(this);
         Button btnStopService = findViewById(R.id.btn_stop_service);
         btnStopService.setOnClickListener(this);
+
+        Button btnBindService = findViewById(R.id.btn_bind_service);
+        btnBindService.setOnClickListener(this);
+
+        Button btnUnbindService = findViewById(R.id.btn_unbind_service);
+        btnUnbindService.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_start_service) {
+        int id = v.getId();
+        if (id == R.id.btn_start_service) {
             Intent startIntent = new Intent(this, MyService.class);
             startService(startIntent);
-        } else if (v.getId() == R.id.btn_stop_service) {
+        } else if (id == R.id.btn_stop_service) {
             Intent stopIntent = new Intent(this, MyService.class);
             stopService(stopIntent);
+        } else if (id == R.id.btn_bind_service) {
+            Intent bindIntent = new Intent(this, MyService.class);
+            bindService(bindIntent, connection, BIND_AUTO_CREATE);
+        } else if (id == R.id.btn_unbind_service) {
+            unbindService(connection);
         }
     }
 }
--- ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -2,19 +2,32 @@ package com.example.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
     private static final String TAG = "MyService";
 
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d(TAG, "startDownload");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress");
+            return 0;
+        }
+    }
+
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
