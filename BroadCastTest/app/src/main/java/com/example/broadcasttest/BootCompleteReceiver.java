67bb62cc241f68a4a8bbce9f9c0be534caf313d1
fansuregrin <pwz113436@gmail.com>
2024-08-27 17:46:31 +0800

    Update BroadCastTest: register broadcast receiver statically

---
+++ BroadCastTest/app/src/main/java/com/example/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,17 @@
+package com.example.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootCompleteReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: Boot Completed");
+        Toast.makeText(context, R.string.boot_complete, Toast.LENGTH_SHORT).show();
+    }
+}
