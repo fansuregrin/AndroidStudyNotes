c4fe908bb06e4b96d04ce52187581f0246ab4c8b
fansuregrin <pwz113436@gmail.com>
2024-08-28 11:20:15 +0800

    Update BroadCastTest: change BroadCast to Broadcast

--- BroadCastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
+++ BroadCastTest/app/src/main/java/com/example/broadcasttest/MyBroadcastReceiver.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class MyBroadCastReceiver extends BroadcastReceiver {
+public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
