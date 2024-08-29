4169102122913542b2b44db1f6d078baf8627ba5
fansuregrin <pwz113436@gmail.com>
2024-08-29 15:34:44 +0800

    Update ServiceTest: foreground service

--- ServiceTest/app/src/main/java/com/example/servicetest/ForegroundService.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/ForegroundService.java
@@ -7,14 +7,18 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
-public class MyService2 extends Service {
+public class ForegroundService extends Service {
+    private static final String TAG = "ForegroundService";
+
     private final String CHANNEL_ID = "com.example.servicetest";
 
-    public MyService2() {
+    public ForegroundService() {
     }
 
     @Override
@@ -25,6 +29,7 @@ public class MyService2 extends Service {
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
         notificationManager.createNotificationChannel(channel);
+        Log.d(TAG, "onCreate");
     }
 
     @Override
@@ -34,9 +39,24 @@ public class MyService2 extends Service {
             .setContentText(getString(R.string.foreground_service))
             .setSmallIcon(R.mipmap.ic_launcher)
             .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+            .setOngoing(true) // 在 API level 34（Android 14） 中无效，用户可以右滑关闭通知
             .build();
-        startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            // 从 API level 34 开始需要指定前台服务类型
+            // 并且启动前台服务时， 系统会根据服务类型检查是否满足特定前提条件
+            startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
+        } else {
+            startForeground(1, notification);
+        }
+        Log.d(TAG, "onStartCommand: startForeground");
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopForeground(Service.STOP_FOREGROUND_REMOVE);
+        Log.d(TAG, "onDestroy: stopForeground");
     }
 
     @Override
--- ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.servicetest;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
 
+    private static final int REQUEST_PERMISSION_CODE = 1;
+
     private MyService.DownloadBinder downloadBinder;
 
-    private ServiceConnection connection = new ServiceConnection() {
+    private final ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "onServiceConnected");
             downloadBinder = (MyService.DownloadBinder) service;
             downloadBinder.startDownload();
             downloadBinder.getProgress();
@@ -26,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            Log.d(TAG, "onServiceDisconnected");
         }
     };
 
@@ -49,8 +57,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnStartForegroundService = findViewById(R.id.btn_start_foreground_service);
         btnStartForegroundService.setOnClickListener(this);
 
+        Button btnStopForegroundService = findViewById(R.id.btn_stop_foreground_service);
+        btnStopForegroundService.setOnClickListener(this);
+
         Button btnStartIntentService = findViewById(R.id.btn_start_intent_service);
         btnStartIntentService.setOnClickListener(this);
+
+        // 检查权限
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.POST_NOTIFICATIONS)
+        != PackageManager.PERMISSION_GRANTED) {
+            String [] permissions = { Manifest.permission.POST_NOTIFICATIONS };
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_PERMISSION_CODE);
+        }
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "onRequestPermissionsResult: the user granted permission POST_NOTIFICATIONS");
+            } else {
+                Log.w(TAG, "onRequestPermissionsResult: the user denied permission POST_NOTIFICATIONS");
+            }
+        }
     }
 
     @Override
@@ -68,8 +101,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (id == R.id.btn_unbind_service) {
             unbindService(connection);
         } else if (id == R.id.btn_start_foreground_service) {
-            Intent intent = new Intent(this, MyService2.class);
+            Intent intent = new Intent(this, ForegroundService.class);
             startForegroundService(intent);
+            Log.d(TAG, "onClick: start foreground service");
+        } else if (id == R.id.btn_stop_foreground_service) {
+            Intent intent = new Intent(this, ForegroundService.class);
+            stopService(intent);
+            Log.d(TAG, "onClick: stop foreground service");
         } else if (id == R.id.btn_start_intent_service) {
             Log.d(TAG, "onClick: thread id is " + Thread.currentThread().getId());
             Intent intent = new Intent(this, MyIntentService.class);
